9a3d918a37cf34813b0c1c16151d18b6fd734f4b
Ruddy Setiadi Gunawan <89178109+ru4871SG@users.noreply.github.com>
2022-07-21 16:05:23 -0500

    first deployment

---
+++ constant/menu.js
@@ -0,0 +1,14 @@
+export const MENUITEMS = [
+   {
+      title: 'Blog', type: 'sub', children: [
+         { path: '/blog/', title: 'Blog Home', type: 'link' },
+         { path: '/blog/posts/review-comparison', title: 'Review Comparison', type: 'link' }
+      ]
+   },
+   {
+      title: 'Pages', type: 'sub', children: [
+         { path: '/page/about-us', title: 'About Us', type: 'link' },
+         { path: '/page/contact', title: 'Support', type: 'link' }
+      ]
+   },
+]
---
+++ pages/index.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react'
+import Head from 'next/head'
+
+// import Custom Components
+import Header from '../containers/common/header'
+import BannerSection from './layouts/sections/event/banner'
+import BookingSection from './layouts/sections/event/booking'
+import AboutSection from './layouts/sections/event/about'
+import SpeakerSection from './layouts/sections/event/speaker'
+import SponserSection from './layouts/sections/event/sponsor'
+import ScheduleSection from './layouts/sections/event/schedule'
+import TestimonialSection from './layouts/sections/event/testimonial'
+import GallerySection from './layouts/sections/event/gallery'
+import CounterSection from './layouts/sections/event/counter'
+import SubscribeSection from './layouts/sections/event/subscribe'
+import PricingSection from './layouts/sections/event/pricing'
+import BlogSection from './layouts/sections/event/blog'
+import FooterSection from './layouts/sections/event/footer'
+
+const Event = () => {
+
+    useEffect(() => {
+        document.body.style.setProperty('--primary', '#e3154f')
+        document.body.style.setProperty('--secondary', '#321575')
+        document.body.style.setProperty('--light', '#321575')
+        document.body.style.setProperty('--dark', '#e3154f')
+    })
+
+    return (
+        <div>
+            <Head>
+                <title>Imusify - Blockchain and Crypto Music Platforms</title>
+            </Head>
+
+            <Header className="event nav-lg" />
+
+            <BannerSection />
+
+            <BookingSection />
+
+            <AboutSection />
+
+            <SpeakerSection />
+
+            <SponserSection />
+
+            <ScheduleSection />
+
+            <TestimonialSection />
+
+            <GallerySection />
+
+            <CounterSection />
+
+            <SubscribeSection />
+
+            <PricingSection />
+
+            <BlogSection />
+
+            <FooterSection />
+
+        </div>
+    )
+}
+
+export default Event;
